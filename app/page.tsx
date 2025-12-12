8d608845b9beee2e55b57e0dd22f91344a38d96b
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2025-12-12 00:24:14 -0800

    logo section begin

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function Home() {
   return (
     <main className="font-jetbrains">
-      <section className="h-screen bg-[#f6efd5] flex flex-row items-center justify-center gap-80">
+      <section className="h-screen flex flex-row items-center justify-center gap-80 border-b-2">
         {/*<div className="w-svw top-0 left-0 absolute z-10">*/}
         {/*  <div className="h-4 w-full bg-[#5EBD3E]"></div>*/}
         {/*  <div className="h-4 w-full bg-[#FFB900]"></div>*/}
@@ -20,8 +20,61 @@ export default function Home() {
           </p>
         </div>
       </section>
-      <section className="h-screen bg-primary border-b-2">
-        <h2>Design Reflections</h2>
+      <section className="h-screen bg-primary border-b-2 flex flex-row" id="design-reflections">
+        <div className="flex flex-col items-center justify-center flex-1">
+          <div className="flex-1 flex items-center justify-center p-4">
+            <h2 className="text-6xl">
+              Design Reflections
+            </h2>
+          </div>
+          <div className="flex-1 h-full w-full bg-red-500 flex flex-row items-center justify-center gap-4 p-4">
+            <div className="flex-1 flex items-center justify-center flex-col gap-4">
+              <h3>DR #1</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+            <div className="flex-1 flex items-center justify-center h-full w-full relative">
+              <Image src="/collage.png" alt="Self Portrait Collage" className="max-h-full max-w-full w-auto h-auto" fill />
+            </div>
+          </div>
+          <div className="flex-1 h-full w-full bg-green-500 p-4"></div>
+        </div>
+        <div className="flex flex-col items-center justify-center flex-1">
+          <div className="flex-1 h-full w-full bg-pink-600 flex flex-row items-center justify-center gap-4 p-4">
+            <div className="flex-1 flex items-center justify-center flex-col gap-4">
+              <h3>DR #1</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+            <div className="flex-1 flex items-center justify-center h-full w-full relative">
+              <Image src="/collage.png" alt="Self Portrait Collage" className="max-h-full max-w-full w-auto h-auto" fill />
+            </div>
+          </div>
+          <div className="flex-1 h-full w-full bg-purple-700 flex flex-row items-center justify-center gap-4 p-4">
+            <div className="flex-1 flex items-center justify-center flex-col gap-4">
+              <h3>DR #1</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+            <div className="flex-1 flex items-center justify-center h-full w-full relative">
+              <Image src="/collage.png" alt="Self Portrait Collage" className="max-h-full max-w-full w-auto h-auto" fill />
+            </div>
+          </div>
+          <div className="flex-1 h-full w-full bg-orange-500 flex flex-row items-center justify-center gap-4 p-4">
+            <div className="flex-1 flex items-center justify-center flex-col gap-4">
+              <h3>DR #1</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+            <div className="flex-1 flex items-center justify-center h-full w-full relative">
+              <Image src="/collage.png" alt="Self Portrait Collage" className="max-h-full max-w-full w-auto h-auto" fill />
+            </div>
+          </div>
+        </div>
       </section>
       <section className="h-screen bg-primary border-b-2 flex flex-row items-center justify-center" id="self-portrait">
         <div className="flex-1 flex justify-center items-center p-20">
@@ -117,8 +170,18 @@ export default function Home() {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
       </section>
-      <section className="h-screen bg-primary border-b-2">
-        <h2>Animated Logo</h2>
+      <section className="h-screen bg-black text-white border-b-2 p-4" id="logo">
+        <h2 className="text-6xl">Animated Logo</h2>
+        <pre>
+          Medium.....................................<br/>
+          Software...................................<br/>
+          Date.......................................<br/>
+          Best Enjoyed...............................Fullscreen, Volume On<br/>
+          Statement..................................
+        </pre>
+        <video controls className="h-full w-auto max-w-1/2 max-h-1/2">
+          <source src="/animated_logo.mp4" type="video/mp4" />
+        </video>
       </section>
       <section className="h-screen bg-primary border-b-2">
         <h2>Short Form Video Montage</h2>
